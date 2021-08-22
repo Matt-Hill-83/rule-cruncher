281811d5fba4e7c35eed53b436ba4355be93449f
Matt Hill <matt.hill.email@gmail.com>
2021-08-22 00:00:25 -0700

    3rd draggable table; works

--- src/Components/RuleMaker/RuleMaker.tsx
+++ src/Components/RuleMaker/RuleMaker.tsx
@@ -2,12 +2,13 @@ import React from "react"
 
 import css from "./RuleMaker.module.scss"
 
-import MyTable from "Components/MyTable/MyTable"
+import MyTable2 from "Components/MyTable2/MyTable2"
+// import MyTable from "Components/MyTable/MyTable"
 
 export default function RuleMaker() {
   return (
     <div className={css.main}>
-      <MyTable />
+      <MyTable2 />
     </div>
   )
 }
