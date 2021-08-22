36e6e8419f6000d8371539af297a237bf7b588c5
Matt Hill <matt.hill.email@gmail.com>
2021-08-21 23:22:56 -0700

    imported dnd table

---
+++ src/Components/MyTable/MyTable copy.tsx
@@ -0,0 +1,59 @@
+import React from "react"
+
+import css from "./MyTable.module.scss"
+
+import MyMultiSelect from "Components/MultiSelect/MultiSelect"
+import MaterialTable, { MTableToolbar } from "material-table"
+
+const listItems = [
+  { title: "Alligator", year: 1994 },
+  { title: "Bunny", year: 1972 },
+  { title: "Caterpillar", year: 1974 },
+]
+
+const multiSelectProps = { listItems, className: css.multiPicker }
+
+export default function MyTable() {
+  return (
+    <div className={css.main}>
+      <MaterialTable
+        options={{
+          toolbar: false,
+        }}
+        components={{
+          Toolbar: (props) => (
+            <div style={{ backgroundColor: "#e8eaf5" }}>
+              <MTableToolbar {...props} />
+            </div>
+          ),
+          // Toolbar: (props) => (
+          //   <div style={{ backgroundColor: "#e8eaf5" }}>
+          //     <MTableToolbar {...props} />
+          //   </div>
+          // ),
+        }}
+        columns={[
+          { title: "Adı", field: "name" },
+          { title: "Year", field: "birthYear", type: "numeric" },
+          {
+            field: "url",
+            title: "Avatar",
+            render: () => (
+              <div style={{ width: 50, borderRadius: "50%" }}>
+                <MyMultiSelect {...multiSelectProps} />
+              </div>
+            ),
+          },
+        ]}
+        data={[
+          {
+            name: "Mehmet",
+            surname: "Baran",
+            birthYear: 1987,
+          },
+        ]}
+        title="Demo Title"
+      />
+    </div>
+  )
+}
--- src/Components/MyTable/MyTable.tsx
+++ src/Components/MyTable/MyTable.tsx
@@ -1,59 +1,242 @@
 import React from "react"
+import styled from "styled-components"
+import { useTable } from "react-table"
+import { DndProvider, useDrag, useDrop } from "react-dnd"
+import update from "immutability-helper"
 
-import css from "./MyTable.module.scss"
+import makeData from "./makeData"
+import { HTML5Backend } from "react-dnd-html5-backend"
 
-import MyMultiSelect from "Components/MultiSelect/MultiSelect"
-import MaterialTable, { MTableToolbar } from "material-table"
+const Styles = styled.div`
+  padding: 1rem;
 
-const listItems = [
-  { title: "Alligator", year: 1994 },
-  { title: "Bunny", year: 1972 },
-  { title: "Caterpillar", year: 1974 },
-]
+  table {
+    border-spacing: 0;
+    border: 1px solid black;
 
-const multiSelectProps = { listItems, className: css.multiPicker }
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
 
-export default function MyTable() {
+    th,
+    td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+`
+
+const Table = ({ columns, data }: { columns: any; data: any }) => {
+  const [records, setRecords] = React.useState(data)
+
+  const getRowId = React.useCallback((row) => {
+    return row.id
+  }, [])
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({
+      data: records,
+      columns,
+      getRowId,
+    })
+
+  const moveRow = (dragIndex: any, hoverIndex: any) => {
+    const dragRecord = records[dragIndex]
+    setRecords(
+      update(records, {
+        $splice: [
+          [dragIndex, 1],
+          [hoverIndex, 0, dragRecord],
+        ],
+      })
+    )
+  }
+
+  const renderedRow: any = (row: any, index: number) => {
+    if (prepareRow(row) !== null && prepareRow(row) !== undefined) {
+      return prepareRow(row)
+    } else {
+      return (
+        <Row index={index} row={row} moveRow={moveRow} {...row.getRowProps()} />
+      )
+    }
+  }
+
   return (
-    <div className={css.main}>
-      <MaterialTable
-        options={{
-          toolbar: false,
-        }}
-        components={{
-          Toolbar: (props) => (
-            <div style={{ backgroundColor: "#e8eaf5" }}>
-              <MTableToolbar {...props} />
-            </div>
-          ),
-          // Toolbar: (props) => (
-          //   <div style={{ backgroundColor: "#e8eaf5" }}>
-          //     <MTableToolbar {...props} />
-          //   </div>
-          // ),
-        }}
-        columns={[
-          { title: "Adı", field: "name" },
-          { title: "Year", field: "birthYear", type: "numeric" },
-          {
-            field: "url",
-            title: "Avatar",
-            render: () => (
-              <div style={{ width: 50, borderRadius: "50%" }}>
-                <MyMultiSelect {...multiSelectProps} />
-              </div>
-            ),
-          },
-        ]}
-        data={[
-          {
-            name: "Mehmet",
-            surname: "Baran",
-            birthYear: 1987,
-          },
-        ]}
-        title="Demo Title"
-      />
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              <th></th>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map(
+            (row, index) => {
+              return renderedRow(row)
+            }
+            // (row, index) =>
+            //   prepareRow(row) || (
+            //     <Row
+            //       index={index}
+            //       row={row}
+            //       moveRow={moveRow}
+            //       {...row.getRowProps()}
+            //     />
+            //   )
+          )}
+        </tbody>
+      </table>
+    </DndProvider>
   )
 }
+
+const DND_ITEM_TYPE = "row"
+
+const Row = ({
+  row,
+  index,
+  moveRow,
+}: {
+  row: any
+  index: number
+  moveRow: any
+}) => {
+  const dropRef: any = React.useRef(null)
+  const dragRef: any = React.useRef(null)
+
+  const [, drop] = useDrop({
+    accept: DND_ITEM_TYPE,
+    hover(item: any, monitor) {
+      if (!dropRef.current) {
+        return
+      }
+      const dragIndex = item.index
+      const hoverIndex = index
+      // Don't replace items with themselves
+      if (dragIndex === hoverIndex) {
+        return
+      }
+      // Determine rectangle on screen
+      const hoverBoundingRect: any = dropRef.current.getBoundingClientRect()
+      // Get vertical middle
+      const hoverMiddleY =
+        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
+      // Determine mouse position
+      const clientOffset: any = monitor.getClientOffset()
+      // Get pixels to the top
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top
+      // Only perform the move when the mouse has crossed half of the items height
+      // When dragging downwards, only move when the cursor is below 50%
+      // When dragging upwards, only move when the cursor is above 50%
+      // Dragging downwards
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return
+      }
+      // Dragging upwards
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return
+      }
+      // Time to actually perform the action
+      moveRow(dragIndex, hoverIndex)
+      // Note: we're mutating the monitor item here!
+      // Generally it's better to avoid mutations,
+      // but it's good here for the sake of performance
+      // to avoid expensive index searches.
+      item.index = hoverIndex
+    },
+  })
+
+  const [{ isDragging }, drag, preview] = useDrag({
+    type: "any",
+    item: { type: DND_ITEM_TYPE, index },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  })
+
+  const opacity = isDragging ? 0 : 1
+
+  preview(drop(dropRef))
+  drag(dragRef)
+
+  return (
+    <tr ref={dropRef} style={{ opacity }}>
+      <td ref={dragRef}>move</td>
+      {row.cells.map((cell: any) => {
+        return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+      })}
+    </tr>
+  )
+}
+
+const App = () => {
+  const columns: any = React.useMemo(
+    () => [
+      {
+        Header: "ID",
+        accessor: "id",
+      },
+      {
+        Header: "Name",
+        columns: [
+          {
+            Header: "First Name",
+            accessor: "firstName",
+          },
+          {
+            Header: "Last Name",
+            accessor: "lastName",
+          },
+        ],
+      },
+      {
+        Header: "Info",
+        columns: [
+          {
+            Header: "Age",
+            accessor: "age",
+          },
+          {
+            Header: "Visits",
+            accessor: "visits",
+          },
+          {
+            Header: "Status",
+            accessor: "status",
+          },
+          {
+            Header: "Profile Progress",
+            accessor: "progress",
+          },
+        ],
+      },
+    ],
+    []
+  )
+
+  const data = React.useMemo(() => makeData(20), [])
+
+  return (
+    <Styles>
+      <Table columns={columns} data={data} />
+    </Styles>
+  )
+}
+
+export default App
