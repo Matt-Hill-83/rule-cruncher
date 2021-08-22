fe186a24af8f5344a5a0edd92f7fc82a1df9441d
Matt Hill <matt.hill.email@gmail.com>
2021-08-21 19:49:20 -0700

    sd dropdown to table

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -5,21 +5,38 @@ import css from "./Dashboard.module.scss"
 import Spacer from "../Spacer/Spacer"
 import RuleMaker from "Components/RuleMaker/RuleMaker"
 import MaterialTable from "material-table"
+import MyMultiSelect from "Components/MultiSelect/MultiSelect"
+
+const listItems = [
+  { title: "Alligator", year: 1994 },
+  { title: "Bunny", year: 1972 },
+  { title: "Caterpillar", year: 1974 },
+]
+
+const multiSelectProps = { listItems, className: css.multiPicker }
 
 function Dashboard() {
   return (
     <div className={css.main}>
       <div className={css.instructions}>
-        <div style={{ maxWidth: "50%" }}>
+        <div style={{ maxWidth: "100%" }}>
           <MaterialTable
             columns={[
               { title: "Adı", field: "name" },
-              { title: "Soyadı", field: "surname" },
-              { title: "Doğum Yılı", field: "birthYear", type: "numeric" },
+              { title: "Year", field: "birthYear", type: "numeric" },
               {
-                title: "Doğum Yeri",
+                title: "City",
                 field: "birthCity",
                 lookup: { 34: "İstanbul", 63: "Şanlıurfa" },
+              },
+              {
+                field: "url",
+                title: "Avatar",
+                render: () => (
+                  <div style={{ width: 50, borderRadius: "50%" }}>
+                    <MyMultiSelect {...multiSelectProps} />
+                  </div>
+                ),
               },
             ]}
             data={[
