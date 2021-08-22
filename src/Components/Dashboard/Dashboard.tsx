8091d587f955241798d906026cf86b98126b66e0
Matt Hill <matt.hill.email@gmail.com>
2021-08-21 19:16:23 -0700

    add material-table

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -4,11 +4,36 @@ import css from "./Dashboard.module.scss"
 
 import Spacer from "../Spacer/Spacer"
 import RuleMaker from "Components/RuleMaker/RuleMaker"
+import MaterialTable from "material-table"
 
 function Dashboard() {
   return (
     <div className={css.main}>
-      <div className={css.instructions}></div>
+      <div className={css.instructions}>
+        <div style={{ maxWidth: "100%" }}>
+          <MaterialTable
+            columns={[
+              { title: "Adı", field: "name" },
+              { title: "Soyadı", field: "surname" },
+              { title: "Doğum Yılı", field: "birthYear", type: "numeric" },
+              {
+                title: "Doğum Yeri",
+                field: "birthCity",
+                lookup: { 34: "İstanbul", 63: "Şanlıurfa" },
+              },
+            ]}
+            data={[
+              {
+                name: "Mehmet",
+                surname: "Baran",
+                birthYear: 1987,
+                birthCity: 63,
+              },
+            ]}
+            title="Demo Title"
+          />
+        </div>
+      </div>
       <Spacer />
       <div className={css.controls}>
         <RuleMaker />
