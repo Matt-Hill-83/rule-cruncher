b78608c6628766b499308239066ff257376a13af
Matt Hill <matt.hill.email@gmail.com>
2021-08-22 22:53:50 -0700

    asfd

--- src/Components/OutputTable/OutputTable.tsx
+++ src/Components/OutputTable/OutputTable.tsx
@@ -5,18 +5,19 @@ import DataGrid from "react-data-grid"
 import { IInputRow } from "Components/RuleMaker/types"
 
 import css from "./OutputTable.module.scss"
-import { AnimalNames } from "Constants/General"
+import { AnimalNames, RestaurantNames } from "Constants/General"
 
 const multiSelectInputWidth = 200
 
 function convertIntToBinary(num: number, digits: number) {
-  var out = ""
-  while (digits--) out += (num >> digits) & 1
-  return out
+  var outputString = ""
+  while (digits--) outputString += (num >> digits) & 1
+  return outputString
 }
 
 const processRows = (rows: IInputRow[]) => {
-  const allPossibleRows = []
+  console.log("rows", rows) // zzz
+  const allPossibleRows: IInputRow[] = []
 
   const animalNames = [
     AnimalNames.ALLIGATOR,
@@ -26,12 +27,28 @@ const processRows = (rows: IInputRow[]) => {
 
   const numAnimals = animalNames.length
   const numRows = Math.pow(2, numAnimals)
-  console.log("numRows", numRows) // zzz
 
-  const test = convertIntToBinary(11, numAnimals)
-  console.log("test", test) // zzz
+  const binaries = []
 
-  return rows
+  for (let index = 0; index < numRows; index++) {
+    const test = convertIntToBinary(index, numAnimals).split("")
+    binaries.push(test)
+  }
+
+  binaries.forEach((binary) => {
+    binary.forEach((digit) => {
+      const newRow = {
+        [AnimalNames.ALLIGATOR]: digit === "1" ? true : false,
+        [AnimalNames.BUNNY]: digit === "1" ? true : false,
+        [AnimalNames.CAT]: digit === "1" ? true : false,
+        restaurant: RestaurantNames.APPLEBEES,
+      }
+      allPossibleRows.push(newRow)
+    })
+  })
+  console.log("allPossibleRows", allPossibleRows) // zzz
+  return allPossibleRows
+  // return rows
 }
 
 export default function OutputTable(props: any) {
